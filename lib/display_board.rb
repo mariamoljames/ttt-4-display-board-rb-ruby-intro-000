d0ceb247a6251b00a2efecd6c6396ae759a2cafc
lite-helper-5188 <mariamol.james@gmail.com>
2020-03-03 03:50:50 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,8 +1,8 @@
 def display_board(board)
-  puts " #{{board[0]}}  | #{{board[0]}}  | #{{board[0]}}  "
+  puts " #{{board[0]}} | #{{board[0]}} | #{{board[0]}} "
   puts "-----------"
-  puts " #{{board[0]}} | #{{board[0]}}  | #{{board[0]}}  "
+  puts " #{{board[0]}} | #{{board[0]}} | #{{board[0]}} "
   puts "-----------"
-  puts " #{{board[0]}}  |  #{{board[0]}} | #{{board[0]}} "
+  puts " #{{board[0]}} | #{{board[0]}} | #{{board[0]}} "
 end# Define display_board that accepts a board and prints
 # out the current state.
